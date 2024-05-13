5c65d9caf5e4e10363e1f14bb6e3d00b906aa3c6
fnal4hire <rubenpangestu14@gmail.com>
2024-05-13 15:13:13 +0700

    html page added

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="navbar">
+    /*display time */
+    </div>
+    <div class="dashboard">
+        <div class="upper half">
+            <div class="map">/*diplay map and pin points*/</div>
+            <div class="detail">/*display details of the pin points*/</div>
+        </div>
+        <div class="second half">
+            <div class="grafik">/*display floodgate height*/</div>
+            <div class="weather x status">
+                <div class="forecast">/*dsiplay weather forecast*/</div>
+                <div class="status">/*planning on progress, display the status either floodgate or the water pump*/</div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
