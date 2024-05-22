2f8c9f34907b9190401e830740eee13e00b13393
fnal4hire <rubenpangestu14@gmail.com>
2024-05-22 12:31:43 +0700

    added scrappign script (WIP)

---
+++ scrapper.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import schedule
+import time
+import csv
+
+def scrape_floodgate_data():
+    url='https://bpbd.jakarta.go.id/waterlevel'
+    page= requests.get(url)
+    soup=bs(page.text,'html')
+    table=soup.find('table')
+    #loads the raw table with html tags
+    with open('floodgate_data.html', 'w') as file:
+        file.write(str(table))
+    headers = []
+    rows = []
+    for header in table.find_all('tr')[1].find_all('td')[1:]:  # Skip the first cell which is empty
+        headers.append(header.get_text(strip=True))
+
+    for row in table.find_all('tr')[2:]:  # Skip the header rows
+        cells = row.find_all('td')
+        if len(cells) > 1:
+            row_data = [cell.get_text(strip=True).replace('\xa0', ' ') for cell in cells[1:]]  # Skip the first cell
+            rows.append(row_data)
+    with open('floodgate_data.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(headers)
+        writer.writerows(rows)
+schedule.every(1).hours.do(scrape_floodgate_data)
