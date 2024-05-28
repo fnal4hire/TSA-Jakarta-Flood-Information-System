d68660253d532272145abd77468afd7b4ce5a3ee
fnal4hire <rubenpangestu14@gmail.com>
2024-05-28 16:17:23 +0700

    fixed backend

--- app.py
+++ app.py
@@ -1,47 +1,64 @@
-from flask import Flask, render_template, send_file, jsonify
-from data_scraper import scrape_floodgate_data, create_floodgate_graph
+from flask import Flask, render_template, request
 import schedule
 import time
 import threading
 import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from scrapper import scrape_floodgate_data  # Sesuaikan nama file jika berbeda
 
-app = Flask(__name__)
-
+app= Flask(__name__)
+#default floodgate_value
+pintu_air="Bendung Katulampa"
 @app.route('/')
 def index():
-    return render_template('index.html')
+    global pintu_air
+    if request.method=='POST':
+        pintu_air=request.form['pintu_air']
+    #graph
+    graph=create_graph(pintu_air)
+    #table
+    dict_list=scrape_floodgate_data()
+    return render_template('index.html',table=dict_list, plot_path=graph)
 
-@app.route('/floodgate_table')
-def floodgate_table():
-    with open('floodgate_data.html', 'r', encoding='utf-8') as file:
-        table_html = file.read()
-    return table_html
+def scrape_and_update():
+    global pintu_air
+    create_graph(pintu_air)
+    scrape_floodgate_data()
 
-@app.route('/floodgate_graph/<pintu_air>')
-def floodgate_graph(pintu_air):
-    buf = create_floodgate_graph(pintu_air)
-    return send_file(buf, mimetype='image/png')
-
-@app.route('/floodgate_data/<pintu_air>')
-def floodgate_data(pintu_air):
-    filename = f'{pintu_air.replace(" ", "_")}.csv'
-    data = []
-    if os.path.exists(filename):
-        with open(filename, mode='r') as file:
-            reader = csv.reader(file)
-            headers = next(reader)
-            data.append(headers)
-            for row in reader:
-                data.append(row)
-    return jsonify({"headers": data[0], "data": data[1:]})
-
-def run_schedule():
+def scheduler():
+    schedule.every().hour.do(scrape_and_update)
     while True:
         schedule.run_pending()
         time.sleep(1)
 
-if __name__ == '__main__':
-    scrape_floodgate_data()
-    schedule.every(1).hours.do(scrape_floodgate_data)
-    threading.Thread(target=run_schedule).start()
+def create_graph(pintu_air):
+    #search for file
+    file= os.path.join('.\csv', f'{pintu_air.replace(" ","_")}.csv')
+    #if exist do
+    if os.path.exists(file):
+        #read file
+        df=pd.read_csv(file)
+        times=df["Time"]
+        levels=df["water Level (cm)"].astype(int)
+        #plotting
+        plt.figure(figsize=(10,5))
+        plt.plot(times, levels, marker='o')
+        plt.suptitle(f"{pintu_air}", fontsize=18)
+        plt.grid(True)
+
+        #save plot
+        dir='.\graph'
+        os.makedirs(dir, exist_ok=True)
+        gpath=os.path.join(dir,f"{pintu_air.replace(" ","_")}.png")
+        plt.savefig(gpath, format='png')
+        plt.close()
+        return gpath
+    return None
+if __name__=='__main__':
+    scheduler_thread = threading.Thread(target=scheduler)
+    scheduler_thread.daemon = True
+    scheduler_thread.start()
+    
+    # Run the Flask app
     app.run(debug=True)
